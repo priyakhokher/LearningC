aad7a57c60dee7b075c33c6ae0dd4687271cc681
Khokher <khokher@amazon.com>
2017-10-07 11:56:24 -0700

    excercises for Chapter1

--- Chapter1/ex1_1.c
+++ Chapter1/ex1_1.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // remove this header file, program still run but gives a warning
+// to either explicitly declare printf or include this header as this contains I/O functions
 
-int main(){
+main(){ // exclude int, will give warning again on no type specifier mentioned, defaults to int, but program still runs 
 
 	printf("%s","n" );
 }
---
+++ Chapter1/ex1_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(){
+
+	printf("%c",'\c' );
+	printf("\n");
+}
